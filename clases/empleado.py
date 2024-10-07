6cf420d1a6e8b85995c8be2ef5e311f6a30a0a48
Ignacio Huiza <ignaciohuiza@gmail.com>
2024-10-07 11:35:53 -0300

    Cambios de ignacio huiza: 1.1

---
+++ clases/empleado.py
@@ -0,0 +1,16 @@
+class empleado():
+    def __init__(self,id_empleado,rut,nombre,direccion,telefono,correo_P,fecha_inicio,salario,id_tipoempleado):
+        self.id_empleado = id_empleado
+        self.rut = rut
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.correo = correo_P
+        self.fecha_inicio = fecha_inicio
+        self.salario = salario
+        self.id_tipoempleado = id_tipoempleado
+
+    def validar_rut():
+        pass
+    def fecha_contrato():
+        pass
