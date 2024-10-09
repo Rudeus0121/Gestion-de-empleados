bb2694dc56e7ee885bef3b00d47726e953a36447
Martyn Mellado <martynmellado@gmail.com>
2024-10-09 11:16:42 -0300

    Cambios Ignacio Huiza 1.1

---
+++ Gestion Empleados/proyeco.py
@@ -0,0 +1,10 @@
+class proyecto():
+    def __init__(self,id_proyecto,nombre_proyecto,descripcion,fecha_inicio,fecha_fin):
+        self.id_proyecto = id_proyecto
+        self.nombre_proyecto = nombre_proyecto
+        self.descripcion = descripcion
+        self.fecha_inicio = fecha_inicio
+        self.fecha_fin = fecha_fin
+    
+    def validar_fechas():
+        pass
---
+++ Gestion Empleados/tipo_empleado.py
@@ -0,0 +1,6 @@
+import empleado
+class tipo_empleado():
+    def __init__(self,id_tipoempleado,tipo_empleado,detalle_empleado):
+        self.id_tipoempleado = id_tipoempleado
+        self.id_tipoempleado = tipo_empleado
+        self.detalle_empleado = detalle_empleado
