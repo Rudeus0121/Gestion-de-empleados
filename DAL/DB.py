791460edf0c51659cb2e9f3a926e43595065add0
Martyn Mellado <martynmellado@gmail.com>
2024-10-23 05:28:55 -0300

    Cambios en base de datos Martyn Mellado 1.4

--- DAL/DB.py
+++ DAL/DB.py
@@ -5,17 +5,20 @@ cnx = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="")
+    password="",
+    database="gestión de empleados")
 
 # Get a cursor
-cur = cnx.cursor()
+cursor = cnx.cursor()
 
 # Execute a query
-cur.execute("SELECT CURDATE()")
+cursor.execute("SHOW TABLES")
+for table in cursor:
+    print(table)
 
-# Fetch one result
-row = cur.fetchone()
-print("Current date is: {0}".format(row[0]))
+#    Fetch one result
+#    row = cursor.fetchone()
+#    print("Current date is: {0}".format(row[0]))
 
-# Close connection
+#Close connection
 cnx.close()
